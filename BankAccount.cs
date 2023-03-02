bba7bb8aa3f9fde3a2c6ee833e6a3869179e2931
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-03-02 14:57:41 -0500

    added class

---
+++ BankAccount.cs
@@ -0,0 +1,13 @@
+public class BankAccount {
+
+    public string customerName = "Bob";
+    public double balance = 0.00;
+    public string typeOfAccount = "checking";
+
+    // default constructor
+    public BankAccount(){
+        customerName = "Bobathan";
+        balance = 300;
+        typeOfAccount = "checking";
+    }
+}
--- program.cs
+++ program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+BankAccount newaccount = new BankAccount();
+// newaccount.customerName = "Jim";
+
+if(newaccount.typeOfAccount == "checking") {
+    // balance = balance + 100;
+    newaccount.balance += 100;
+}
+
+
+
+Console.WriteLine(newaccount.customerName + "'s balance is $" + newaccount.balance);
